94ed18a4c56f82ab332c8554830f6649f3c3ec37
Melissa Zellhuber <melissa.zellhuber@gmail.com>
2023-11-13 18:48:30 -0600

    Initial Commit

---
+++ RGB/RGBApp.swift
@@ -0,0 +1,17 @@
+//
+//  RGBApp.swift
+//  RGB
+//
+//  Created by Melissa Zellhuber on 13/11/23.
+//
+
+import SwiftUI
+
+@main
+struct RGBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
