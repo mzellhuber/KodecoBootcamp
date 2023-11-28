0903a85e357faddc5964fb7a960079f047fc532d
Melissa Zellhuber <melissa.zellhuber@gmail.com>
2023-11-28 15:47:44 -0600

    Extracted Views

--- RGB/ContentView.swift
+++ RGB/ContentView.swift
@@ -11,48 +11,99 @@ struct ContentView: View {
     @State private var redValue: Double = 128
     @State private var greenValue: Double = 128
     @State private var blueValue: Double = 128
-    @State private var rgbText: String = ""
-    
+    @State private var buttonColor: Color = Color.blue
+
     var body: some View {
+        GeometryReader { geometry in
+            let isLandscape = geometry.size.width > geometry.size.height
+            if isLandscape {
+                landscapeView
+            } else {
+                portraitView
+            }
+        }
+    }
+
+    var titleAndInstructions: some View {
         VStack {
             Text("RGB")
                 .font(.largeTitle)
                 .fontWeight(.bold)
                 .padding()
-            
-            RoundedRectangle(cornerRadius: 15)
-                .fill(Color(red: redValue / 255, green: greenValue / 255, blue: blueValue / 255))
-                .frame(height: 200)
-                .shadow(radius: 10)
-                .padding(.horizontal)
-            
+
             Text("Adjust the sliders to mix your color")
                 .font(.subheadline)
                 .padding(.top)
+        }
+    }
+
+    var portraitView: some View {
+        VStack {
+            titleAndInstructions
+            ColorDisplayView(redValue: redValue, greenValue: greenValue, blueValue: blueValue)
+            slidersAndButton
+        }
+    }
+
+    var landscapeView: some View {
+        HStack {
+            VStack {
+                titleAndInstructions
+                ColorSlider(value: $redValue, textColor: .red)
+                ColorSlider(value: $greenValue, textColor: .green)
+                ColorSlider(value: $blueValue, textColor: .blue)
+                SetColorButton(color: buttonColor, action: changeButtonColor)
+            }
             
+            VStack {
+                ColorDisplayView(redValue: redValue, greenValue: greenValue, blueValue: blueValue)
+            }
+        }
+    }
+
+    var slidersAndButton: some View {
+        VStack {
             ColorSlider(value: $redValue, textColor: .red)
             ColorSlider(value: $greenValue, textColor: .green)
             ColorSlider(value: $blueValue, textColor: .blue)
-            
-            if !rgbText.isEmpty {
-                Text(rgbText)
-                    .font(.headline)
-                    .padding()
-            }
-            
-            Button(action: {
-                rgbText = "RGB(\(Int(redValue)), \(Int(greenValue)), \(Int(blueValue)))"
-            }) {
-                Text("Set Color")
-                    .foregroundColor(.white)
-                    .frame(maxWidth: .infinity)
-                    .padding()
-                    .background(Color.blue)
-                    .cornerRadius(10)
-                    .shadow(radius: 5)
-            }
-            .padding()
+            SetColorButton(color: buttonColor, action: changeButtonColor)
         }
+    }
+
+    func changeButtonColor() {
+        buttonColor = Color(red: redValue / 255, green: greenValue / 255, blue: blueValue / 255)
+    }
+}
+
+struct ColorDisplayView: View {
+    let redValue: Double
+    let greenValue: Double
+    let blueValue: Double
+
+    var body: some View {
+        RoundedRectangle(cornerRadius: 15)
+            .fill(Color(red: redValue / 255, green: greenValue / 255, blue: blueValue / 255))
+            .frame(height: 200)
+            .shadow(radius: 10)
+            .padding()
+    }
+}
+
+struct SetColorButton: View {
+    var color: Color
+    var action: () -> Void
+
+    var body: some View {
+        Button(action: action) {
+            Text("Set Color")
+                .foregroundColor(.white)
+                .frame(maxWidth: .infinity)
+                .padding()
+                .background(color)
+                .cornerRadius(10)
+                .shadow(radius: 5)
+        }
+        .padding()
     }
 }
 
