994b2ab73788a97f1de1e151e58ff2678f498f59
Melissa Zellhuber <melissa.zellhuber@gmail.com>
2024-03-10 17:41:06 -0600

    Better details

--- week9/Pexels Viewer/Pexels Viewer/View/DetailView.swift
+++ week9/Pexels Viewer/Pexels Viewer/View/DetailView.swift
@@ -9,17 +9,79 @@ import SwiftUI
 
 struct DetailView: View {
     let photo: Photo
+    @State private var showLarge2X = false
 
     var body: some View {
-        VStack {
-            AsyncImage(url: URL(string: photo.src.large)) { image in
-                image.resizable()
-                     .aspectRatio(contentMode: .fit)
-            } placeholder: {
-                ProgressView()
+        ScrollView {
+            VStack {
+                Button(action: { showLarge2X.toggle() }) {
+                    Text(showLarge2X ? "1x" : "2x")
+                        .foregroundColor(.white)
+                        .padding()
+                        .background(Color.blue)
+                        .cornerRadius(10)
+                        .padding()
+                }
+
+                if showLarge2X {
+                    AsyncImage(url: URL(string: photo.src.large2X)) { phase in
+                        switch phase {
+                        case .empty:
+                            ProgressView()
+                        case .success(let image):
+                            image
+                                .cornerRadius(10)
+                                .padding()
+                        case .failure:
+                            Image(systemName: "photo.on.rectangle.angled")
+                                .resizable()
+                                .scaledToFit()
+                                .foregroundColor(.secondary)
+                                .padding()
+                        @unknown default:
+                            EmptyView()
+                        }
+                    }
+                } else {
+                    AsyncImage(url: URL(string: photo.src.large)) { phase in
+                        switch phase {
+                        case .empty:
+                            ProgressView()
+                        case .success(let image):
+                            image
+                                .resizable()
+                                .scaledToFit()
+                                .cornerRadius(10)
+                                .padding()
+                        case .failure:
+                            Image(systemName: "photo.on.rectangle.angled")
+                                .resizable()
+                                .scaledToFit()
+                                .foregroundColor(.secondary)
+                                .padding()
+                        @unknown default:
+                            EmptyView()
+                        }
+                    }
+                }
+
+                VStack(alignment: .center, spacing: 8) {
+                    Text(photo.photographer)
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .padding(.top)
+
+                    Text(photo.alt)
+                        .font(.body)
+                        .foregroundColor(.secondary)
+                }
+                .padding()
+                .background(Color.white)
+                .cornerRadius(10)
+                .padding([.horizontal, .bottom])
             }
         }
-        .navigationTitle(photo.photographer)
-        .padding()
+        .navigationBarTitleDisplayMode(.inline)
+        .padding(.horizontal)
     }
 }
