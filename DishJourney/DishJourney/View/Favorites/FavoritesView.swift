f46fb81dba649eb6a9eb7e6b2284d8ca74dd1cca
Melissa Zellhuber <melissa.zellhuber@gmail.com>
2024-05-25 17:30:01 -0600

    Empty state

--- DishJourney/DishJourney/View/Favorites/FavoritesView.swift
+++ DishJourney/DishJourney/View/Favorites/FavoritesView.swift
@@ -12,18 +12,47 @@ struct FavoritesView: View {
 
   var body: some View {
     NavigationView {
-      List {
-        ForEach(viewModel.favorites, id: \.uri) { recipe in
-          NavigationLink(destination: RecipeDetailView(recipe: recipe)) {
-            RecipeRow(recipe: recipe)
-          }
+      VStack {
+        if viewModel.favorites.isEmpty {
+          emptyStateView
+        } else {
+          favoritesListView
         }
-        .onDelete(perform: viewModel.removeFavorites)
       }
       .navigationTitle("Favorites")
       .toolbar {
         EditButton()
       }
+    }
+  }
+
+  private var emptyStateView: some View {
+    VStack {
+      Image(systemName: "heart")
+        .resizable()
+        .frame(width: 100, height: 100)
+        .foregroundColor(.gray)
+        .padding()
+      Text("No Favorites Yet")
+        .font(.title)
+        .foregroundColor(.gray)
+      Text("You can add recipes to your favorites by tapping the heart icon on the recipe row or detail page.")
+        .font(.body)
+        .foregroundColor(.gray)
+        .multilineTextAlignment(.center)
+        .padding(.horizontal)
+    }
+    .padding()
+  }
+
+  private var favoritesListView: some View {
+    List {
+      ForEach(viewModel.favorites, id: \.uri) { recipe in
+        NavigationLink(destination: RecipeDetailView(recipe: recipe)) {
+          RecipeRow(recipe: recipe)
+        }
+      }
+      .onDelete(perform: viewModel.removeFavorites)
     }
   }
 }
